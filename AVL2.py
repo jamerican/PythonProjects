fc929db7b917bdb8b51363e1acb6f4a759859962
Kamau <Miscman227@hotmail.com>
2022-01-12 14:26:48 -0500

    -Added Balance Method Skeleton -Added Delete Methos Skeleton

--- AVL2.py
+++ AVL2.py
@@ -8,6 +8,7 @@ class AVL_TREE:
     def __init__(self):
         self.root = None
 
+    #Insert Methods
     def insert(self, key):
         if self.root == None:
             self.root = Node(key)
@@ -20,13 +21,20 @@ class AVL_TREE:
             return Node(key)
         if key < root.key:
             root.left = self.insert_helper(root.left, key)
-            #self.update(root)
+            self.update(root)
             return True
         else:
             root.right = self.inset_helper(root.right, key)
-            #self.update(root)
+            self.update(root)
             return True
         return False
+
+    #Delete Method
+    def delete(self, key):
+        pass
+
+    def delete(self, root, key):\
+        pass
 
 
     #BR Update Methods
@@ -43,5 +51,29 @@ class AVL_TREE:
             right_tree = 1 + self.height(root.right)
             return max(left_tree, right_tree)
 
-    #Balance Methods
+    #Balance Method
+    def Balance(self, root):
+        if -1 <= root.bf <= 1:
+            return
+        else:
+            if root.bf == -2:
+                if root.left.bf == -1:
+                    self.LeftLeftRotate(root)
+                else:
+                    self.LeftRightRotate(root)
+            if root.bf == 2:
+                if root.right == 1:
+                    self.RightRightRotate(root)
+                else:
+                    self.RightLeftRotate(root)
+
+    #Rotation Methods
+    def LeftLeftRotate(self, root):
+        pass
+    def LeftrightRotate(self, root):
+        pass
+    def RightRightRotate(self, root):
+        pass
+    def RightLeftRotate(self, root):
+        pass
 
