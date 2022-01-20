97fd44eed589e8b54d064831131b7a2498423a96
Kamau <Miscman227@hotmail.com>
2022-01-20 14:05:07 -0500

    -Added Parent Helper method -Updated Formatting to group "Helper Method" -Updated "Update" and "Balance" methods -Changed syntax for conditional statements

--- AVL2.py
+++ AVL2.py
@@ -10,22 +10,22 @@ class AVL_TREE:
 
     #Insert Methods
     def insert(self, key):
-        if self.root == None:
+        if self.root is None:
             self.root = Node(key)
             return True
         else:
             return self.insert_helper(self.root, key)
 
     def insert_helper(self, root, key):
-        if root == None:
+        if root is None:
             return Node(key)
         if key < root.key:
             root.left = self.insert_helper(root.left, key)
-            self.update(root)
+            #self.update(root)
             return True
         else:
             root.right = self.inset_helper(root.right, key)
-            self.update(root)
+            #self.update(root)
             return True
         return False
 
@@ -40,30 +40,23 @@ class AVL_TREE:
     #BR Update Methods
     def update(self, root):
         root.bf = self.height(root.right) - self.height(root.left)
-
-    def height(self, root):
-        if not root.left and not root.right:
-            return 0
-        else:
-            left_tree = 1 + self.height(root.left)
-            right_tree = 1 + self.height(root.right)
-            return max(left_tree, right_tree)
-
-    #Balance Method
-    def Balance(self, root):
-        if -1 <= root.bf <= 1:
+        if -1 >= root.bf <= 1:
             return
         else:
-            if root.bf == -2:
-                if root.left.bf == -1:
-                    self.LeftLeftRotate(root)
-                else:
-                    self.LeftRightRotate(root)
-            if root.bf == 2:
-                if root.right == 1:
-                    self.RightRightRotate(root)
-                else:
-                    self.RightLeftRotate(root)
+            self.balance(root)
+
+    #Balance Method
+    def balance(self, root):
+        if root.bf == -2:
+            if root.left.bf == -1:
+                self.LeftLeftRotate(root)
+            else:
+                self.LeftRightRotate(root)
+        elif root.bf == 2:
+            if root.right == 1:
+                self.RightRightRotate(root)
+            else:
+                self.RightLeftRotate(root)
 
     #Rotation Methods
     def LeftLeftRotate(self, root):
@@ -96,4 +89,11 @@ class AVL_TREE:
             else:
                 return self.parent(key, root.right)
 
+    def height(self, root):
+        if root.left is None and root.right is None:
+            return 0
+        else:
+            left_tree = 1 + self.height(root.left)
+            right_tree = 1 + self.height(root.right)
+            return max(left_tree, right_tree)
 
